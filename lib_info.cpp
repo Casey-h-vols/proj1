5165a3cdb7ef79cc87400ccb7507668d18599b65
parkb77 <parkerbabb7@gmail.com>
2026-08-26 14:04:03 -0400

    Update lib_info.cpp

--- lib_info.cpp
+++ lib_info.cpp
@@ -51,12 +51,12 @@ int main (int argc, char* argv[]) {
 
        if (ss >> songTitle >> songTime >> artistName >> albumName >> songGenre >> songNum) {
            
-        data[artistName].albums[albumName].songs[songTitle].title = songTitle;
-        data[artistName].albums[albumName].songs[songTitle].time = songTime; 
+        data[artistName].albums[albumName].songs[songNum].title = songTitle;
+        data[artistName].albums[albumName].songs[songNum].time = songTime; 
         data[artistName].name = artistName; 
         data[artistName].albums[albumName].name = albumName;
-        data[artistName].albums[albumName].songs[songTitle].genre = songGenre;
-        data[artistName].albums[albumName].songs[songTitle].trackNum = songNum;
+        data[artistName].albums[albumName].songs[songNum].genre = songGenre;
+        data[artistName].albums[albumName].songs[songNum].trackNum = songNum;
 
             
        }
