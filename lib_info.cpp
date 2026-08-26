bc917bbc3cb94954ec92b2523ec42f8d7b2a4d4a
parkb77 <parkerbabb7@gmail.com>
2026-08-26 12:40:02 -0400

    Update lib_info.cpp

--- lib_info.cpp
+++ lib_info.cpp
@@ -1,1 +1,22 @@
+#include <string>
+#include <iostream>
+#include <fstream>
 
+struct Song { 
+    string title;
+    int time;  // could also be a string
+};
+
+struct Album {
+    map <int, Song > songs;
+    string name;
+    int time;
+    int nsongs;  // optional variable but makes it easier
+};
+
+struct Artist {
+    map <string, Album > albums;
+    string name;
+    int time;
+    int nsongs;
+};
