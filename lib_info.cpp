97a6bcead95d609eb63cd5b5c7155b0e1ae5fb2d
Casey Alexander Higgins <chiggi18@tesla26.eecs.utk.edu>
2026-08-31 10:37:34 -0400

    74 out of 100 correct on this version I expect some formatting issues.

--- lib_info.cpp
+++ lib_info.cpp
@@ -30,6 +30,21 @@ struct Artist {
 };
 
 
+string convert_time(int a){
+	int sec,min;
+
+	if ((a / 60)>0){
+		sec = a%60;
+		min = a/60;
+		if (sec < 10){
+			return to_string(min)+":0"+to_string(sec);
+		} else return to_string(min)+":"+to_string(sec);
+	}
+	sec = a%60;
+	return "0:"+to_string(sec);
+	 
+}
+
 
 // Main func needs to 
 int main (int argc, char* argv[]) { 
@@ -75,7 +90,7 @@ int main (int argc, char* argv[]) {
 			timeVec.push_back(stoi(minStr));
 		}
 		sec = (timeVec[0]*60) + timeVec[1];
-		cout << "TIME: " << sec << "\n";
+		//cout << "TIME: " << sec << "\n";
            
            
         //Note: song map key is track number not title, helps when sorting numerically
@@ -83,6 +98,8 @@ int main (int argc, char* argv[]) {
         data[artistName].albums[albumName].songs[songNum].time = sec;
 		data[artistName].albums[albumName].totalTime += sec;
 		data[artistName].totalTime += sec;
+		data[artistName].nsongs++;
+		data[artistName].albums[albumName].nsongs++;
 		data[artistName].name = artistName; 
         data[artistName].albums[albumName].name = albumName;
         data[artistName].albums[albumName].songs[songNum].genre = songGenre;
@@ -98,7 +115,23 @@ int main (int argc, char* argv[]) {
     inputFile.close();
 
     //Print data 
-    
+	
+	map<string, Artist>::iterator itr1;
+	map<string, Album>::iterator itr2;
+	map<int, Song>::iterator itr3;
+
+	for (itr1 = data.begin(); itr1 != data.end(); ++itr1) {
+		cout << itr1->first <<": " << itr1->second.nsongs << ", " << convert_time(itr1->second.totalTime) <<endl;
+	
+		for (itr2 = itr1->second.albums.begin(); itr2 != itr1->second.albums.end(); ++itr2){
+			cout <<"        " << itr2->first << ": "<< itr2->second.nsongs << ", " << convert_time(itr2->second.totalTime) << endl;
+
+			for (itr3 = itr2->second.songs.begin(); itr3 != itr2->second.songs.end(); ++itr3){
+				cout << "                "  << itr3->first << ". " << itr3->second.title << ": " << convert_time(itr3->second.time) << endl;
+			}
+		}
+	}
+
     return 0; 
 }
 
